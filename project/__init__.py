ebe708dbb99fe6a2532c78fb468d90d494a13233
Zachary Chandler <chandlerzach@seattleu.edu>
2022-03-09 05:39:29 -0800

    implement a cannonical email field
    email addresses are caseless, but interpreting user input in this senses seems fraught.
    The 'email' attribute can be used for display, and cannonical_email can be used for uniqueness.

--- project/__init__.py
+++ project/__init__.py
@@ -1,9 +1,9 @@
-from enum import unique
-from flask import Flask, request, jsonify, g, session, redirect, abort, url_for
+from flask import Flask, request, jsonify, redirect, abort, url_for
 from flask_login import UserMixin, LoginManager, login_user, current_user, login_required, logout_user
 import bcrypt
 import flask_sqlalchemy
 import os
+
 
 lm = LoginManager()
 
@@ -11,14 +11,15 @@ def create_app(test_config=None):
   app = Flask(__name__)
   app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
   app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or '8aa7260d1028ae38f6e978dee5a1e0e80c02bc94de74096e'
+  app.config['SQLALCHEMY_ECHO'] = True
   db = flask_sqlalchemy.SQLAlchemy(app)
   lm.init_app(app)
 
-  
-  class User(UserMixin, db.Model):
+  class User(UserMixin, db.Model): #UserMixin provides authentication defaults, db.Model makes this class into an ORM
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True, nullable=False)
+    cannonical_email = db.Column(db.String, unique=True, nullable=False)
     password_hash = db.Column(db.LargeBinary, nullable=False)
 
     def __repr__(self):
@@ -32,13 +33,13 @@ def create_app(test_config=None):
 
   @lm.unauthorized_handler
   def unauthorized():
-    return abort('401', description="Not authorized")
+    return abort(401, description="Not authorized")
 
   @app.route("/login")
   def login():
     email = request.json['email']
     password = request.json['password']
-    real_user = User.query.filter_by(email=email).first()
+    real_user = User.query.filter_by(cannonical_email=email.lower()).first()
     if real_user and bcrypt.checkpw(password.encode('utf-8'), real_user.password_hash):
       login_user(real_user)
       return redirect(url_for('secrets'), code=418)
@@ -60,11 +61,11 @@ def create_app(test_config=None):
   @app.route("/signup", methods=["POST"])
   def signUp():
     email = request.json['email']
-    if User.query.filter_by(email=email).first(): return '400'
+    if User.query.filter_by(cannonical_email=email.lower()).first(): return '400'
     password = request.json['password']
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(password.encode('utf-8'), salt) #this step silently ignores passwords longer than 72 characters, and we are goingto treat all input as utf-8 for now
-    db.session.add(User(email=email, password_hash=hash))
+    db.session.add(User(email=email, password_hash=hash, cannonical_email=email.lower()))
     db.session.commit()
     return '200'
 
