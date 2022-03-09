f201fb1b31b8e1c57987c5c927aad606d16e0494
Zachary Chandler <chandlerzach@seattleu.edu>
2022-03-09 02:33:28 -0800

    user creation and basic authenticaiton working

--- project/__init__.py
+++ project/__init__.py
@@ -1,10 +1,10 @@
 from enum import unique
 from flask import Flask, request, jsonify, g
-
+import bcrypt
 import flask_sqlalchemy
 import os
 import psycopg2
-
+from flask_login import LoginManager
 
 
 
@@ -18,9 +18,10 @@ def create_app(test_config=None):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
+    password_hash = db.Column(db.LargeBinary, nullable=False)
+
     def __repr__(self):
-      return f"{self.id} {self.email} {self.password}"
+      return f"{self.id} {self.email} {self.password_hash}"
     
 
   db.create_all()
@@ -28,17 +29,28 @@ def create_app(test_config=None):
   @app.route("/signup", methods=["POST"])
   def signUp():
     email = request.json['email']
-    password = request.json['password'] #TODO hash this
-    db.session.add(User(email=email, password=password))
+    password = request.json['password']
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(password.encode('utf-8'), salt) #this step silently ignores passwords longer than 72 characters, and we are goingto treat all input as utf-8 for now
+    db.session.add(User(email=email, password_hash=hash))
     db.session.commit()
     return '200'
     
+  @app.route("/login")
+  def login():
+    email = request.json['email']
+    password = request.json['password']
+    real_user = User.query.filter_by(email=email).first()
+    if real_user and bcrypt.checkpw(password.encode('utf-8'), real_user.password_hash):
+      #do something? auth token of some kind?
+      return '200'
+    return '401'
 
 
   @app.route("/users")
   def getAll():
     all_users = db.session.query(User).all()
-    ret = [{'id': user.id, 'email': user.email, 'password': user.password} for user in all_users]
+    ret = [{'id': user.id, 'email': user.email, 'password_hash': user.password_hash.decode('utf-8')} for user in all_users]
     return jsonify(ret)
 
 
