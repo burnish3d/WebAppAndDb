a71a02cd425b61f0f0647297028be5648846bb38
Zachary Chandler <chandlerzach@seattleu.edu>
2022-03-09 01:28:36 -0800

    replace manual SQL with ORM Traded out psycopg2 for flask_sqlalchemy, there were no evident gains from the more manual process

--- project/__init__.py
+++ project/__init__.py
@@ -1,75 +1,46 @@
+from enum import unique
 from flask import Flask, request, jsonify, g
-from flask_sqlalchemy import SQLAlchemy
+
+import flask_sqlalchemy
 import os
 import psycopg2
 
 
 
 
-def emailExists(cursor, email):
-  lowercase = email.lower()
-  query = "SELECT EXISTS (SELECT 1 FROM users WHERE email=%(user_input)s);"
-  cursor.execute(query, {"user_input": lowercase})
-  return cursor.fetchone()[0] == 1
-
-def insertUser(curs, email, password_digest):
-  query = """
-  INSERT INTO users (email, password_digest)
-  VALUES (%(email)s, %(password_digest)s);
-  """
-  curs.execute(query, {'email': email, 'password_digest': password_digest})
-
-
-def digestPassword(password):
-  return int(password)
-
 
 def create_app(test_config=None):
   app = Flask(__name__)
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+  db = flask_sqlalchemy.SQLAlchemy(app)
 
-  def getDB():
-    db = getattr(g, '_database', None)
-    if db is None:
-      user = os.environ['POSTGRES_USER']
-      database = os.environ['POSTGRES_DB']
-      password = os.environ['POSTGRES_PASSWORD']
-      host = 'db'
-      db = g._database = psycopg2.connect(host=host, dbname=database, user=user, password=password)
-    return db
+  class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    def __repr__(self):
+      return f"{self.id} {self.email} {self.password}"
+    
 
-  @app.teardown_appcontext
-  def teardown_db(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-      db.commit() #this is here to make transactions commit, the docs suggest this should happen when a cursor is used as a context manager though so I'm unsure of what is wrong
-      db.close()
+  db.create_all()
 
-  @app.route("/signup", methods=["PUT"])
+  @app.route("/signup", methods=["POST"])
   def signUp():
     email = request.json['email']
-    password = request.json['password']
-    conn = getDB()
-    with conn.cursor() as curs:
-      if emailExists(curs, email) == False:
-        digest = digestPassword(password) #TODO that auth and password stuff
-        print(email, digest)
-        insertUser(curs, email, digest) #should function return value or otherwise handle errors here?
-        return '200'
-      else:
-        return '400'
+    password = request.json['password'] #TODO hash this
+    db.session.add(User(email=email, password=password))
+    db.session.commit()
+    return '200'
     
 
 
   @app.route("/users")
   def getAll():
-    conn = getDB()
-    with conn.cursor() as curs:
-      query = "SELECT * FROM users;"
-      ret = ''
-      curs.execute(query)
-      for row in curs.fetchall():
-        ret += str(row) + '<br>'
-      return ret
+    all_users = db.session.query(User).all()
+    ret = [{'id': user.id, 'email': user.email, 'password': user.password} for user in all_users]
+    return jsonify(ret)
+
 
   @app.route('/')
   def index():
